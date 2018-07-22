5e173332dc9979c5b73db123a7fffadff97fe5a9
Wessel Prins <wesselprins@gmail.com>
2018-07-22 19:19:02 +0200

    add install of zsh

--- setupscript.sh
+++ setupscript.sh
@@ -1,7 +1,9 @@
 #!
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-git clone https://github.com/seebi/dircolors-solarized.gita ~/dotfiles/dircolors-solarized
+git clone https://github.com/seebi/dircolors-solarized.git ~/dotfiles/dircolors-solarized
 
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
