9b4ef70996f2ce0b77bbd720819098377ab19f1f
Wessel Prins <wesselprins@gmail.com>
2019-03-30 12:55:32 +0100

    updated setup script with syntax highlighting

--- setupscript.sh
+++ setupscript.sh
@@ -1,9 +1,15 @@
-#!
+#!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-git clone https://github.com/seebi/dircolors-solarized.git ~/dotfiles/dircolors-solarized
+#git clone https://github.com/seebi/dircolors-solarized.git ~/dotfiles/dircolors-solarized
 
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+
+rm -r $HOME/.oh-my-zsh/custom
+
+ln -s $HOME/dotfiles/custom $HOME/.oh-my-zsh/custom
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
